eb366fffd48140c34e3300f15b39c7cf51c38367
borjasolerr <borjasolrey@gmail.com>
2019-01-24 15:55:28 +0100

    Added instructor to lambda classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -12,3 +12,20 @@ class Person {
     console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
   }
 }
+/*------- Instructor -------*/
+class Instructor extends Person {
+  constructor(options) {
+    super(options);
+    this.speciality = options.speciality;
+    this.favLanguage = options.favLanguage;
+    this.catchPhrase = options.catchPhrase;
+  }
+  demo(subject) {
+    console.log(`'​​​​​Today we are learning about ${subject}'`);
+  }
+  grade(student, subject) {
+    console.log(
+      `'​​​​​${student.name} receives a perfect score on ${subject}'`
+    );
+  }
+}
