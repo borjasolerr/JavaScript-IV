9f4d258a77758f6a1b12de2a470200a9b0f226dd
borjasolerr <borjasolrey@gmail.com>
2019-01-24 15:52:02 +0100

    Added Person to lambda-classes.js

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,14 @@
 // CODE here for your Lambda Classes
+
+/*------- Person -------*/
+class Person {
+  constructor(options) {
+    this.name = options.name;
+    this.age = options.age;
+    this.location = options.location;
+    this.gender = options.gender;
+  }
+  speak() {
+    console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
+  }
+}
