2bf8c6365a2307c53ae0ca103c7149481814f614
borjasolerr <borjasolrey@gmail.com>
2019-01-24 16:19:42 +0100

    Added Instructors, studends and PM in lambda classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -63,3 +63,74 @@ class ProjectM extends Instructor {
     console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
   }
 }
+// INSTRUCTORS
+const homer = new Instructor({
+  name: "Homer",
+  age: 40,
+  location: "San Francisco",
+  gender: "M",
+  speciality: "React",
+  favLanguage: "JavaScript",
+  catchPhrase: "Don't forget the docs"
+});
+
+const fred = new Instructor({
+  name: "Fred",
+  age: 37,
+  location: "Bedrock",
+  gender: "M",
+  specialty: "Back-end",
+  favLanguage: "JavaScript",
+  catchPhrase: `Don't forget the homies`
+});
+
+// STUDENTS
+const borja = new Student({
+  name: "Borja",
+  age: 24,
+  location: "Ibiza",
+  gender: "M",
+  previousBackground: "Entrepreneur",
+  className: "WEBEU1",
+  favSubjects: ["Git", "LESS", "JavaScript"]
+});
+
+const maria = new Student({
+  name: "Maria",
+  age: 23,
+  location: "London",
+  gender: "F",
+  previousBackground: "Designer",
+  className: "WEBEU1",
+  favSubjects: ["Sass", "JavaScript", "React"]
+});
+
+// PROJECT MANAGERS
+const sarah = new ProjectM({
+  name: "Sarah",
+  age: 34,
+  location: "San Francisco",
+  gender: "F",
+  gradClassName: "CS39",
+  favInstructor: "Fred"
+});
+
+const michel = new ProjectM({
+  name: "Michel",
+  age: 34,
+  location: "New York",
+  gender: "M",
+  gradClassName: "CS1",
+  favInstructor: "Fred"
+});
+
+homer.demo("React");
+homer.grade(maria, "JS array methods");
+fred.demo("Front-end");
+fred.grade(borja, "JS classes");
+
+borja.PRAssignment("JS Project");
+maria.sprintChallenge("JS Fundamentals");
+
+michel.standUp("WEBUE1");
+sarah.debugsCode(maria, "the this keyword");
